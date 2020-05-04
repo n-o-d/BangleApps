6791c3e6dc1e87efad4502dd139c88558e347909
n-o-d <alexander.bisler@gmx.de>
2020-05-04 17:36:50 +0200

    Update d3fender.app.js
    comment

--- apps/d3fender/d3fender.app.js
+++ apps/d3fender/d3fender.app.js
@@ -1,1 +1,3 @@
 E.showMessage("D-F3nDAR","Presse BTN1 to start");
+
+// icon from <a target="_blank" href="https://icons8.com/icons/set/space-shuttle">Space Shuttle icon</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
