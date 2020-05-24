926dfa78c5eb70ccc650495ba90ef8cdbf012413
n-o-d <alexander.bisler@gmx.de>
2020-05-24 16:42:50 +0200

    many bugfixes

--- apps/ghops/ghops.app.js
+++ apps/ghops/ghops.app.js
@@ -21,6 +21,10 @@ https://www.andreas-rozek.de/Bangle.js/index_de.html
 
 */
 
+var useFakeGPS = true; // 4dbg set to true
+var createFakeGPSOnlyOnce = 0; // 4dbg
+
+
 var W = g.getWidth();
 var H = g.getHeight();
 
@@ -37,7 +41,10 @@ var curLoc_subgrid; // coords of location!
 var newLoc_grid; // newly incoming data
 var newLoc_subgrid;
 
-var curLocation;
+var curLong_gps; // last measured GPS coordinates; only valid if hasGPSSignal is true
+var curLat_gps;
+
+var curLocation; // Location object
 
 var imageGhost = new Uint8Array([
       0b00111100,
@@ -53,7 +60,9 @@ var imageGhost = new Uint8Array([
 
 // ================================================
 
-var gridWidth = 3; // unit? degrees? mircominiarcsecondsw WTF?
+var gridWidth = 40; //4dbg  - use 100
+//unit? degrees? mircominiarcsecondsw WTF?
+// 10 = 1m ???
 var locationVisibilityRange = gridWidth/3; // unit: subgrid?
 var gpsPrecisionFactor = 10000.0;
 
@@ -71,8 +80,8 @@ class GridCoordinates {
 	
 	initFromGPS(long_gps_i, lat_gps_i) { 
        // TEST
-      var lon_grid = Math.floor(long_gps_i * gpsPrecisionFactor) % gridWidth;
-      var lat_grid = Math.floor(lat_gps_i  * gpsPrecisionFactor) % gridWidth;
+      var lon_grid = Math.floor(long_gps_i * gpsPrecisionFactor) / gridWidth;
+      var lat_grid = Math.floor(lat_gps_i  * gpsPrecisionFactor) / gridWidth;
       
 	  this.long_grid = lon_grid;
 	  this.lat_grid = lat_grid;
@@ -90,11 +99,17 @@ class GridCoordinates {
 	
 	// @returns true if coordinates are the same
 	compare( other) { 
-		if (other.long_grid == this.long_grid && other.lat_grid && this.lat_grid)
+		if (other.long_grid == this.long_grid && 
+            other.lat_grid  == this.lat_grid)
 			return true; // same coordinates
 		else
 			return false;
 	}
+  
+    toString() {
+      var s = this.long_grid.toString() + " " + this.lat_grid.toString();
+      return s;
+    }
 }
 
 // ================================================
@@ -113,8 +128,8 @@ class SubgridCoordinates {
 	
 	initFromGPS( long_gps_i,  lat_gps_i) { 
        // TEST
-      var lon_subgrid = Math.floor(long_gps_i * gpsPrecisionFactor);
-      var lat_subgrid = Math.floor(lat_gps_i  * gpsPrecisionFactor);
+      var lon_subgrid = Math.floor(long_gps_i * gpsPrecisionFactor) % gridWidth;
+      var lat_subgrid = Math.floor(lat_gps_i  * gpsPrecisionFactor) % gridWidth;
       
 	  this.long_subgrid = lon_subgrid;
 	  this.lat_subgrid  = lat_subgrid;
@@ -127,11 +142,17 @@ class SubgridCoordinates {
 	
 	// @returns true if coordinates are the same
 	compare( other) { 
-		if (other.long_subgrid == this.long_subgrid && other.lat_subgrid && this.lat_subgrid)
+		if (other.long_subgrid == this.long_subgrid &&
+            other.lat_subgrid  == this.lat_subgrid)
 			return true; // same coordinates
 		else
 			return false;
 	}
+  
+    toString() {
+      var s = this.long_subgrid.toString() + " " + this.lat_subgrid.toString();
+      return s;
+    }
 }
 
 // ================================================
@@ -178,22 +199,42 @@ class RND {
 
 // ================================================
 
+const ItemType = {
+  undefined: 0,
+  ghopFood : 1,
+  psyEnergyCell: 2
+};
+
 class Item {
 
 	constructor() {
 		this.defined = false;
 		this.name = "";
-		this.type = 0;
+		this.namePlural = "";
+		this.type = 0; // see ItemType
 		this.value1 = 0;
 		this.quantity = 1;
 	}
 	
-	generate(seed, TODO ) { 
-		RND.setSeed(seed+111);
+	generate(seed /*, TODO*/ ) { 
+		var rnd = new RND(); 
+        rnd.setSeed(seed+111);
 		
+      
+       console.log("Item::generating");
+      // 4dbg
+      
+		this.defined = true;
+		this.name = "Ghop food";
+		this.namePlural = "Ghop foods";
+		this.type = ItemType.ghopFood;
+      
+      
 	}
 	
+    isDefined() { return this.defined; }
 	getName() { return this.name; }
+	getNamePlural() { return this.namePlural; }
 	getType() { return this.type; }
 	getValue1() { return this.value1; }
 	getQuantity() { return this.quantity; }
@@ -264,7 +305,8 @@ class NPC {
 	}
 	
 	generate(seed, TODO ) { 
-		RND.setSeed(seed+11);
+		var rnd = new RND(); 
+        rnd.setSeed(seed+11);
 		
 	}
 	
@@ -295,9 +337,12 @@ const LocationType = {
 };
 
 const LocationProbability = {
-  empty: 0.2,  // 4bdg  should be higher
+  empty: 0.2,  // 4dbg - TODO:  should be higher
   tower: 0.4,
-  hauntedHouse: 0.4
+  hauntedHouse: 0.4,
+  
+  itemPresent: 0.3,
+  NPCPresent: 0.2
 };
 
 class Location {
@@ -314,7 +359,7 @@ class Location {
 		
 		// constants
 		
-		this.maxOffset_subgrid = 30; // for point of interest
+		this.maxOffset_subgrid = 1; // for point of interest
 	
 	}
 	
@@ -328,14 +373,34 @@ class Location {
 		this.curItem = null; // new Item();
     }
   
+	calcPointOfInterest_subgrid(coords_grid, seed) {
+	    var rnd = new RND(); 
+        rnd.setSeed(seed);
+
+		
+		
+		var longOffset_subgrid = rnd.randomFloat(-this.maxOffset_subgrid, this.maxOffset_subgrid);
+		var latOffset_subgrid  = rnd.randomFloat(-this.maxOffset_subgrid, this.maxOffset_subgrid);
+		
+		var newCoords_subgrid = new SubgridCoordinates();
+		newCoords_subgrid.long_subgrid = longOffset_subgrid + coords_grid.long_grid;
+		newCoords_subgrid.lat_subgrid  = latOffset_subgrid  + coords_grid.lat_grid;
+		
+		return newCoords_subgrid;
+	}
+  
 	generate(seed, coords_grid) { 
       var localSeed = seed+1;
-	  RND.setSeed(localSeed);
+	  var rnd = new RND(); 
+      rnd.setSeed(localSeed);
 	
       var probEmptySpace = 0.4;
       probEmptySpace = 0.2; // <<<<< 4dbg
       
-      var diceForType = RND.random();
+      var diceForType = rnd.random();
+      var diceForNPC  = rnd.random();
+      var diceForItem = rnd.random();
+      
       if (diceForType <= LocationProbability.empty) {
         this.type = LocationType.empty;
       } else {
@@ -348,19 +413,37 @@ class Location {
         }
       }
       
-     //TODO var diceForNPC = ...;
-     //TODO var diceForItem = ...;
+      // 4dbg  -  just generate locations in a pattern
+      if (coords_grid.long_grid % 2 == 0)
+        this.type = LocationType.tower;
+      else
+        this.type = LocationType.hauntedHouse;
+      //4dbg
+      
+      
+      
       
       // set cellCoords_grid , coordinates_subgrid
       
       this.cellCoords_grid.copyFrom(coords_grid);
       
-      var rndCoords_subgrid = calcPointOfInterest_subgrid(this.cellCoords_grid, localSeed);
+      var rndCoords_subgrid = this.calcPointOfInterest_subgrid(this.cellCoords_grid, localSeed);
       this.coordinates_subgrid.copyFrom(rndCoords_subgrid);
       
       if (this.type != LocationType.empty) {
         // set optionally  NPC or item
       
+        if (diceForItem <= LocationProbability.itemPresent 
+            || true /* 4dbg */) {
+          
+          console.log("generating new item...");
+          
+          var newItem = new Item();
+          newItem.generate(localSeed);
+	      this.putItemHere(newItem);
+        }
+        
+        
         // TODO
       }
 	}
@@ -384,25 +467,20 @@ class Location {
       }
     }
 	
+    isItemPresent() { 
+      if (this.curItem != null && this.curItem.isDefined())
+        return true
+      else
+        return false;
+    }
+    getItem() { return this.curItem; }
 	putItemHere(item) { this.curItem = item; }
+  
 	putNPCHere (npc) { this.curNPC = npc; }
 	
 	removeItem() { this.curItem = null; }
 	removeNPC () { this.curNPC = null; }
 	
-	calcPointOfInterest_subgrid(coords_grid, seed) {
-		RND.setSeed(seed);
-		
-		
-		var longOffset_subgrid = RND.randomFloat(-this.maxOffset_subgrid, this.maxOffset_subgrid);
-		var latOffset_subgrid  = RND.randomFloat(-this.maxOffset_subgrid, this.maxOffset_subgrid);
-		
-		var newCoords_subgrid = new SubgridCoordinates();
-		newCoords_subgrid.long_subgrid = longOffset_subgrid + coords_grid.long_grid;
-		newCoords_subgrid.lat_subgrid  = latOffset_subgrid  + coords_grid.lat_grid;
-		
-		return newCoords_subgrid;
-	}
 }
 
 
@@ -434,18 +512,34 @@ function enableGPS() {
 
     monitoringGPS = true;
     hasGPSSignal = false;
-    Bangle.setGPSPower(true);
-    Bangle.on('GPS', handleGPSValues);
-
+    if (!useFakeGPS) {
+      Bangle.setGPSPower(true);
+      Bangle.on('GPS', handleGPSValues);
+    }
 }
 
 function disableGPS() {
 
     monitoringGPS = false;
     hasGPSSignal = false;
-    Bangle.removeAllListeners('GPS');
-    Bangle.setGPSPower(false);
+    if (!useFakeGPS) {
+      Bangle.removeAllListeners('GPS');
+      Bangle.setGPSPower(false);
+    }
+}
 
+function createFakeGPS() {
+  if (!useFakeGPS)
+    return;
+  
+  curLong_gps = 1;
+  curLat_gps  = 2;
+  newLoc_grid.initFromGPS(curLong_gps, curLat_gps);
+  newLoc_subgrid.initFromGPS(curLong_gps, curLat_gps);
+  
+  hasGPSSignal = true;
+  
+  enteringNewCell();
 }
 
 function handleGPSValues(gpsValueSet) {
@@ -460,6 +554,8 @@ function handleGPSValues(gpsValueSet) {
     return;
   }
 
+  curLong_gps = gpsValueSet.lon;
+  curLat_gps  = gpsValueSet.lat;
   newLoc_grid.initFromGPS(gpsValueSet.lon, gpsValueSet.lat);
   newLoc_subgrid.initFromGPS(gpsValueSet.lon, gpsValueSet.lat);
   
@@ -511,26 +607,52 @@ function enteringNewCell() {
   curLocation = new Location();
   curLocation.generate(cellSeed, curLoc_grid);
   
-  // TODO: here ????
+  // TODO: use visibility range !!
   if (curLocation.type != LocationType.empty) {
     Bangle.buzz();
   }
 }
 
 function displayMainScreen() {
+  
   g.clear();
   
   var msg;
+  var cellCoords = "cc";
   
-  if (hasGPSSignal) {
-    msg = "You see " + curLocation.getDescription() + ".";
+  if (hasGPSSignal && curLocation != null) {
+    msg = "You see " + curLocation.getDescription() + ".\n";
+    
+    if (curLocation.isItemPresent()) {
+      
+      var presentItem = curLocation.getItem();
+      msg += "There ";
+      if (presentItem.getQuantity() > 1) {
+        msg += "are " + presentItem.getQuantity()+ " " + presentItem.getNamePlural() + ".";
+      
+      } else {
+        msg += "is a "+ presentItem.getName() + ".";
+      }
+      msg += "\n"
+
+    } else {
+      msg += "."; // 4dbg
+    }
+    
+    
+    
+    // 4dbg: show cell coords:
+    cellCoords = "Grid: " + curLoc_grid.toString() + "\n";
+    cellCoords +=  "SG: " + newLoc_subgrid.toString();
+    
   } else {
-    msg = "-*= no signal =*-";
+    msg = "no signal :(";
   }
   
-  g.drawString(msg, 10, 30);
+  g.drawString(msg, 1, 30);
   
-  
+  g.setFont("6x8", 2);
+  g.drawString(cellCoords, 1, 100);
   
   g.flip();
 }
@@ -565,13 +687,11 @@ function gameOver() {
   mainInterval = undefined;
   
   disabledGPS();
-  
-  g.setColor(1,1,1);
 
   g.setColor(1,1,1);
   g.setFont("6x8", 2);
 
-  g.drawString("GAME OVER", 10, 20);
+  g.drawString("GAME OVER", 10, 50);
   
   setTimeout(gameOver2, 3*1000);
   
@@ -596,7 +716,18 @@ function mainLoop() {
       
   }
 
+  
+  if (useFakeGPS) {
+    
+    if (createFakeGPSOnlyOnce < 1) {
+      createFakeGPS();
+      createFakeGPSOnlyOnce++;
+    }
+    
+  } 
+    
   displayMainScreen();
+    
   
   //? g.setFontAlign(0,0); // center font
   
@@ -634,7 +765,7 @@ function startGame1() {
     g.setColor(1,1,1);
     g.setFont("6x8", 2);
   
-    g.drawString("Walk towards\n the light", 40, 30);
+    g.drawString("Walk towards\n the light", 10, 30);
     
     g.flip();
     
@@ -674,7 +805,7 @@ function showTitle() {
   g.setFont("6x8", 2);
   
   g.drawString("Ghops", W/2-30, 5);
-  g.drawString("Press 1 to start\n\nWritten by n-o-d\n     and MR", 10, 40);
+  g.drawString("Press 1\nto start\n\nWritten by\nn-o-d and MR", 10, 50);
   
   setWatch(startGame1, BTN1);
   
